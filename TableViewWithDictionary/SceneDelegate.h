86ae8e9561c0c77ded2a4ce4087841ad9203f5d9
RJ <r.j.chomik@gmail.com>
2020-10-01 19:24:27 +0100

    Project created

---
+++ TableViewWithDictionary/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TableViewWithDictionary
+//
+//  Created by The App Experts on 01/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TableViewWithDictionary/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TableViewWithDictionary
+//
+//  Created by The App Experts on 01/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
